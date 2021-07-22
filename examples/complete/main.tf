a9e0dcf6710d3a098c89e084244d19b92c7d2b5b
rm3na <84992903+rm3na@users.noreply.github.com>
2021-07-22 07:27:57 -0600

    Update main.tf

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -8,22 +8,22 @@ module "terraform-intersight-iks" {
   source = "terraform-cisco-modules/iks/intersight"
   # source = "terraform-cisco-modules/iks/intersight//"
   # Infra Config Policy Information
-  cluster_name = "test0614"
+  cluster_name = "POC"
   # cluster_action   = "Deploy"
-  vc_target_name   = "marvel-vcsa.rich.ciscolabs.com"
-  vc_portgroup     = ["panther|triggerMesh|tme"]
-  vc_datastore     = "iks"
-  vc_cluster       = "tchalla"
+  vc_target_name   = "11.11.11.80"
+  vc_portgroup     = ["Terraform-50"]
+  vc_datastore     = "Terraform"
+  vc_cluster       = "HX"
   vc_resource_pool = ""
   vc_password      = var.vc_password
 
   # IP Pool Information
-  ip_starting_address = "10.139.120.220"
+  ip_starting_address = "192.168.70.100"
   ip_pool_size        = "20"
   ip_netmask          = "255.255.255.0"
-  ip_gateway          = "10.139.120.1"
-  ntp_servers         = ["10.101.128.15"]
-  dns_servers         = ["10.101.128.15"]
+  ip_gateway          = "192.168.70.1"
+  ntp_servers         = ["pool.time.org"]
+  dns_servers         = ["8.8.8.8"]
 
   addons_list = [{
     addon_policy_name = "dashboard"
@@ -44,7 +44,7 @@ module "terraform-intersight-iks" {
   # pod_cidr = "100.65.0.0/16"
   # service_cidr = "100.64.0.0/24"
   # cni = "Calico"
-  domain_name = "rich.ciscolabs.com"
+  domain_name = "nterone.com"
   timezone    = "America/New_York"
 
 
